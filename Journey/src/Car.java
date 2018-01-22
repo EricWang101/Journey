87aa6cbcbe1239edd2c17f1947705b057793ba30
Eric Wang <ewang101@terpmail.umd.edu>
2018-01-21 22:19:42 -0500

    Added comments for the Car, and Main class

--- Journey/src/Car.java
+++ Journey/src/Car.java
@@ -1,22 +1,27 @@
 import java.awt.Color;
 
+/**
+ * A concrete class that implements the Vehicle interface; 
+ * Stores data of a single car;
+ * This object will be added to an environment;
+ */
 public class Car implements Vehicle {
 	
-	private static double topSpeed = 4.0; //MARK
-	private static Color color = Color.MAGENTA;
-	protected static final int carHeight = 35;
+	protected static final int carHeight = 35; // The height of the car
+	private static final double topSpeed = 4.0; // The top speed of the car(Pixels per tick)
+	private static final Color color = Color.MAGENTA; // The color of the car
 	
-	private Environment environment; 
-	private double position;
-	private int lane;
-	private double speed; 
-	private double acceleration; 
-	private double brakingPower; 
-	protected boolean passing = false;
-	private Vehicle passedVehicle;
+	private Environment environment; // The environment the car object will be placed in
+	private double position; // The position of the car on the road
+	private int lane; // The lane the car is on 
+	private double speed; // The current speed(Pixels per tick) 
+	private double acceleration; // The acceleration of the car 
+	private double brakingPower; // the braking power of the car 
+	protected boolean passing = false; // Whether or not the car passed another vehicle
+	private Vehicle passedVehicle; // The vehicle that was passed
 	private VehicleState state = VehicleState.CONSTANT;
 	
-	
+	/** The class constructor, which sets up the car properties */
 	public Car(Environment env, double pos, int lane, double speed, double accel, double braking) {
 		this.environment = env;
 		this.position = pos;
@@ -26,103 +31,115 @@ public class Car implements Vehicle {
 		this.brakingPower = braking;	
 	}
 	
+	/** Clones , and returns a new Car object */
 	public Car clone() {
 		return new Car(environment,position,lane,speed,acceleration,brakingPower);
 	}
 	
-
+	/** Updates the behavior of  the car after one unit of time has passed */
 	public void tick(Environment environment) {
 		switch(this.state) {
 			case CONSTANT: 
 				break;
 			case ACCELERATING: 
-				if((speed<topSpeed) && (speed*acceleration<environment.getSpeedLimit())) {
+				if((speed<topSpeed) && (speed*acceleration<environment.getSpeedLimit())) { 
 					if(speed < 1.0) this.speed = 1.0;
 					if(speed*acceleration > environment.getSpeedLimit()) {
 						this.speed = environment.getSpeedLimit();
 					}else {
 						speed = speed*acceleration;
 					}
-					
 				}
 				break;
 			case BRAKING:
 				if(speed > 0) {
 					speed = speed*brakingPower;
 				}
-				break; //THE BUG I FINALLY FOUND IT
+				break; 
 			case CRASHED:
 				speed = 0.0;
 				return;	
 		}
-		position += speed;
-			
+		position += speed; // Adds the speed of the car, which varys depending on the state to the car's position
 	}
 
+	/** Returns the state of the car */
 	@Override
 	public VehicleState getState() {
 		return this.state;
 	}
-
+	
 	@Override
+	/** Sets the state of the car */
 	public void setState(VehicleState state) {
 		this.state = state; 
 	}
-
+	
+	/**Returns the current lane of the car*/
 	@Override
 	public int getLane() {
 		return this.lane;
 	}
-
+	
+	/** Sets the lane of the car*/
 	@Override
 	public void setLane(int lane) {
 		this.lane = lane;	
 	}
 	
+	/** Returns the position of the car*/
 	public double getPosition() {
 		return this.position;
 	}
 
-	
+	/** Returns a boolean value representing whether or not the car's state is CRASHED */
 	public boolean isCrashed() {
 		return state == VehicleState.CRASHED;
 	}
-
+	
+	/** Returns the color of the car*/
 	@Override
 	public Color getColor() {
 		return color;
 	}
 
+	/** Returns the height of the car*/
 	@Override
 	public double getHeight() {
 		return carHeight;
 	}
 	
+	/** Returns a boolean value representing whether or not the car has passed another car*/
 	public boolean isPassing() {
 		return this.passing;
 	}
 
+	/** Return's the top speed of a car */
 	@Override
 	public double getTopSpeed() {
 		return this.topSpeed;
 	}
-
+	
+	/** Return's the current speed of the car */
 	@Override
 	public double getSpeed() {
 		return this.speed;
 	}
 
+	/** Sets whether or not a car has passed another vehicle */
 	@Override
 	public void setPassing(boolean isPassing) {
 		this.passing = isPassing;
 		
 	}
 
+	/** Returns the vehicle that was passed by this object */
 	@Override
 	public Vehicle getPassedVehicle() {
 		return this.passedVehicle;
 	}
 
+	/** Sets the vehicle that was passed by this object */
 	@Override
 	public void setPassedVehicle(Vehicle vehicle) {
 		this.passedVehicle = vehicle;
--- Journey/src/Main.java
+++ Journey/src/Main.java
@@ -7,76 +7,75 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+/**
+ * The main frame of the program, and the most outer container;
+ * @author Eric Wang
+ */
 public class Main extends JFrame{
 	
-	private Environment defaultEnvironment = new SunnyDay();
-	private Display display = new Display(defaultEnvironment);
-	private JMenuBar menuBar = new JMenuBar();
-	private JMenu options = new JMenu("Options");
-	private JMenu gameplay = new JMenu("Settings");
+	private Environment defaultEnvironment = new SunnyDay(); // The default environment the Display object will show
+	private Display display = new Display(defaultEnvironment); // The display object, which takes an environment as a parameter
+	private JMenuBar menuBar = new JMenuBar(); // The JMenuBar which will contain the two JMenu components: "Options", and "Settings" 
+	private JMenu options = new JMenu("Options"); // The JMenu item that will contain the functions reset, pause, and quit 
 	
+	private JMenu settings = new JMenu("Settings"); // Currently not implemented, but will contain the functions related to the environment
 	
+	/** Initializes the main frame */
 	public static void main(String[] args ) {
 		Main main = new Main();
 		main.setResizable(false);
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main.setTitle("Journey");
 		main.setVisible(true);
-		main.setLocationRelativeTo(null);
-		
+		main.setLocationRelativeTo(null);	
 	}
 	
+	/** The class constructor, which sets up the menu bar, and the display */
 	public Main() {
 		defaultEnvironment.setDisplay(display);
 		addRandomCars(defaultEnvironment);
 		setUpOptions();
-		
-	
-	 
+		setUpSettings();
 		this.setJMenuBar(menuBar);
-	
 		this.getContentPane().add(display);
 		pack();
-	
 	}
 	
-	public void addVehicles(Environment environment) {
-		
-	}
-	
+	/**
+	 * Adds the car objects to the environment;
+	 * Each car is initialized with different speeds, acceleration, and braking power; 
+	 * @param environment The environment in which the cars will be added to
+	 */
 	public void addRandomCars(Environment environment) {
 		Random randomNumber = new Random();
 		for(int i = 0; i< 9;i++) {
-			environment.addVehicle(new Car(environment, 
-					Car.carHeight * 1.2*i,
-					randomNumber.nextInt(environment.getLanes()-1),//MAYBE NOT MINUS ONE// Maybe have to add top speed to the class
-					randomNumber.nextInt(10) / 4.0,
-					1.001+(.098) * randomNumber.nextDouble(),
-				    0.95+ (.04) * randomNumber.nextDouble()));
+			environment.addVehicle(new Car(environment, // The environment the car will be added to 
+					Car.carHeight *1.2*i, // The starting position of the car
+					randomNumber.nextInt(environment.getLanes()-1), // The starting lane of the car 
+					randomNumber.nextInt(10) / 4.0, // The speed 
+					1.001+(.098) * randomNumber.nextDouble(), // The acceleration
+				    0.95+ (.04) * randomNumber.nextDouble())); // The braking efficiency 
 		}
 	}
 	
+	/** Sets up the field "options" */
 	public void setUpOptions() {
+		JMenuItem resetOption = new JMenuItem("Reset"); // Creating a reset function
 		
-		JMenuItem resetOption = new JMenuItem("Reset");
 		resetOption.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				defaultEnvironment.clear();
-				display.reset();
-				addRandomCars(defaultEnvironment);
-				defaultEnvironment.resetTimer();
-				
-			}
-			
+				defaultEnvironment.clear(); // Clears the environment of its cars
+				display.reset(); // Clears the yOffset of the display
+				addRandomCars(defaultEnvironment); // Adds random cars to the environment agian
+				defaultEnvironment.resetTimer();	 // Resets the timer
+			}	
 		});
+	
+		options.add(resetOption); // Adding the JMenuItem "reset" to options
+		JMenuItem pause = new JMenuItem("Pause"); // Creating a pause function
 		
-		options.add(resetOption);
-		
-		JMenuItem pause = new JMenuItem("Pause");
 		pause.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(defaultEnvironment.isRunning()) {
@@ -90,20 +89,29 @@ public class Main extends JFrame{
 			}
 			
 		});
-		options.add(pause);
 		
-		JMenuItem quit = new JMenuItem("Quit");
+		options.add(pause); // Adding the JMenuItem "pause" to options
+		JMenuItem quit = new JMenuItem("Quit"); // Creating a quit function
+		
 		quit.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);	
 			}	
 		});
-		options.add(quit);
-		menuBar.add(options);
 		
+		options.add(quit); // Adding the JMenuItem "quit" to options
+		menuBar.add(options); // Adding the JMenu "options" to the JMenuBar
 	}
 	
-
+	/**
+	 * Will be implemented in the future when there are multiple types of vehicles 
+	 * @param environment The environment the vehicles will be added to
+	 */
+    public void addVehicles(Environment environment) { }
+    
+    /** Will be implemented in the future!*/
+    public void setUpSettings() { }
+    
+    
 }
